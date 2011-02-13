fd426f9425d1792b219ef75a69c16c329ee50ecc
payload <payload@lavabit.com>
2011-02-13 04:18:59 +0100

    Added translation.

--- Funny.hs
+++ Funny.hs
@@ -5,18 +5,33 @@ import Vec
 
 data Funny = Funny {
     funnyInside :: Vec3D -> Bool,
+    funnyBounds :: Vec3D -> Bool,
     funnyR :: Double
 }
 
-funnies = [funnyFun 3, funnySphere 3 vec0, funnySphere 3 vecX]
+funnies = [funnyWave 10, funnyDodo 3, funnySphere 3 vec0, funnyFun 3, funnySphere 3 vecX]
 
-funnySphere r pos =
-    Funny inside r
+bounds r v =
+    all between (Vec.toList v)
     where
-        inside v = Vec.normSq (pos-v) < r*r
+        between x = -r < x && x < r
+
+funnySphere r pos = Funny (insideSphere r pos) (bounds r) r
+insideSphere r pos v = Vec.normSq (pos-v) < r*r
+        
+funnyDodo r = Funny inside (bounds r) r
+    where
+        inside v =
+            Vec.normSq ((Vec3D (-r*0.66) 0 0) - v) < r*r &&
+            (not $ Vec.normSq v < r*r)
+            
+funnyWave r = Funny inside (bounds r) r
+    where
+        inside v@(Vec3D x y z) =
+            z < x*y
         
 funnyFun r =
-    Funny inside r
+    Funny inside (bounds r) r
     where
         inside v@(Vec3D x y z) =
             Vec.normSq v < r*r && (not $ x < 0 && y > 0 && z < 0)
--- Voxelizer.hs
+++ Voxelizer.hs
@@ -16,11 +16,10 @@ data Voxel = Voxel {
 } deriving Show
 -- Voxel
 
-voxelize (Funny inside r) step =
+voxelize funny@(Funny inside bounds r) step =
     [Voxel v step s | (v,s) <- zip grid visibleSides, length s > 0]
     where
-        visibleSides = map (voxelVisibleSides step inside) grid
-        --step = (r*2) / int2Double steps
+        visibleSides = map (voxelVisibleSides step funny) grid
         steps = double2Int $ (r*2) / step
         grid = voxelGrid allSteps
         allSteps = voxelStepThrough steps step r
@@ -34,16 +33,15 @@ voxelGrid allSteps =
 voxelStepThrough steps step r = 
     take (steps+1) (iterate (\x -> x+step) (-r))
 
-voxelVisibleSides step inside v
-    | inside v = voxelNeighborsInside step inside v
+voxelVisibleSides step funny@(Funny inside _ _) v
+    | inside v = voxelNeighborsInside step funny v
     | otherwise = []
 
-voxelNeighborsInside :: Double -> (Vec3D -> Bool) -> Vec3D -> [Cube.Side]
-voxelNeighborsInside step inside v =
+voxelNeighborsInside step (Funny inside bounds _) v =
     [s | (s,v) <- zip sides neighborsInside, not v]
     where
-        neighborsInside = map inside $ map (+ v) neighbors
-        neighbors = vecs ++ map negate vecs
+        neighborsInside = map (\e -> inside e && bounds e) neighbors
+        neighbors = map (+ v) (vecs ++ map negate vecs)
         vecs = map (Vec.map (step *)) [vecX, vecY, vecZ]
         sides = [
             Cube.Right, Cube.Top, Cube.Front,
